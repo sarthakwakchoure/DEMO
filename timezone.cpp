e7bb15a2ee96c7934970b0aa1030668725406493
Suraj Jorwekar <surajjorwekar@gmail.com>
2022-05-24 19:56:13 +0530

    Create timezone.cpp

---
+++ timezone.cpp
@@ -0,0 +1,14 @@
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+int main ()
+{
+
+  using sc = std::chrono::system_clock;
+  auto tm = sc::to_time_t(sc::now());
+
+  std::cout << std::put_time(std::localtime(&tm), "formatted time: %Y-%m-%dT%X%z\n");
+
+  std::cout << "just the offset: " << std::put_time(std::localtime(&tm), "%z\n");
+
+}
